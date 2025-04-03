da2ed8c8b621abe874cddcb03f9c70dbd0026858
Subhash Galla <subhashgalla@dynamic-10-125-210-100.dynamic.unt.edu>
2025-04-03 10:16:34 -0500

    created contact page added form

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -1,10 +1,44 @@
 "use client"
 import {motion} from "framer-motion";
+import { Span } from "next/dist/trace";
+import { useState } from "react";
 
 const ContactPage = () => {
+  const [sucess, setSucess] = useState(false)
+  const [error, setError] = useState(false)
+
+  const text = "Say Hello"
   return (
     <motion.div className="h-full" initial={{y:"-200vh"}} animate={{y:"0%"}} transition={{duration: 1}}>
-      Contact
+      <div className="h-full flex flex-col lg:flex-row px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48">
+        {/* text container */}
+        <div className="h-1/2 lg:h-full lg:w-1/2 flex items-center justify-center text-6xl">
+          <motion.dev>
+            {text.split("").map((letter, index)=>
+                (
+                  <motion.span key={index} initial={{opacity:1}} animate={{opacity:0}} transition={{duration:3, repeat:Infinity, delay:index*0.1,}}>
+                      {letter}
+                  </motion.span>
+                )
+            )}
+          </motion.dev>
+
+        </div>
+        {/* form container */}
+        <form className="h-1/2 lg:h-full lg:w-1/2 bg-red-50 rounded-xl text-xl flex flex-col gap-8 justify-center p-24 ">
+            <span>Dear Subhash Galla,</span>
+            <textarea rows={6} className="bg-transparent border-b-2 border-b-black outline-none resize-none"></textarea>
+            <span>
+              My mail address is:
+              <input type="text" className="bg-transparent border-b-2 border-b-black outline-none"/>
+            </span>
+            <span>Regards</span>
+            <button className="bg-purple-200 rounded font-semibold text-gray-600 p-4">Send</button>
+            {sucess && <Span className="text-green-600 font-semibold">Your message has been sent successfully!</Span>}
+            {error && <Span className="text-red-600 font-semibold">Something went wrong</Span>}
+        </form>
+         
+      </div>
     </motion.div>
   )
 }
